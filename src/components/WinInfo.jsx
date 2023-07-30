3553e638051bb2f556868d160f8c461e5d7c5241
koraykoksal <kksl.koray@gmail.com>
2023-07-30 21:37:27 +0300

    champion leage data and show winInfo page

--- src/components/WinInfo.jsx
+++ src/components/WinInfo.jsx
@@ -1,7 +1,52 @@
 import React from 'react'
+import { useEffect,useState } from 'react'
 
-export const WinInfo = () => {
+
+export const WinInfo = ({clyear,championsLeage}) => {
+
+  const [data, setdata] = useState({})
+
+  const [showimage, setshowimage] = useState(true)
+
+  useEffect(() => {
+   
+    championsLeage.map((item,i)=>{
+
+      if(item.winner_year == clyear){
+        setdata(item)
+      }
+  })
+  
+  }, [clyear])
+  
+
+
   return (
-    <div>WinInfo</div>
+
+
+    <>
+      
+
+
+
+
+      {
+        showimage?(
+          <div class="max-w-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-auto">
+
+          <div className='max-w-xl p-3'>
+
+            <h2 className='text-black text-center text-xl p-2'> {data.kazanan_takim_adi}</h2> 
+
+            <img src={data.kazanan_takim_logo} className=' max-h-56 rounded-md m-auto' alt="" />
+
+          </div>
+
+      </div>
+        ):(alert('fff'))
+      }
+
+
+    </>
   )
 }
--- src/components/WinYear.jsx
+++ src/components/WinYear.jsx
@@ -1,25 +1,38 @@
 import React from 'react'
 import {championsLeage} from "../helper/cl.js"
-
+import { useState } from 'react'
+import { WinInfo } from './WinInfo.jsx'
 
 
 export const WinYear = () => {
 
+  const [clyear, setclyear] = useState('2023')
+  //const [cldata, setcldata] = useState(championsLeage)
+ 
 
   return (
 
     <div>
     
-        <div className={`mt-[3vh] p-3 mx-auto overflow-hidden relative w-[800px] h-[80px] rounded-[8px] bg-green-700 blur-[0.5px] flex flex-wrap justify-between`}>
+        <div className={`mt-[3vh] p-3 mx-auto overflow-hidden relative w-[800px] h-[80px] rounded-[8px]  flex flex-wrap justify-between`}>
 
         {
           championsLeage.map((item,i)=>(
-            <button value={item?.winner_year} onClick={(e)=>{console.log(e.target.value)}}>{item?.winner_year}</button>
+
+            <button type="button" class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            value={item?.winner_year} onClick={(e)=>{setclyear(e.target.value)}}>{item?.winner_year}</button>
+
           ))
         }
           
         </div>
 
+
+        <WinInfo clyear={clyear} championsLeage={championsLeage}/>
+
       </div>
+
+
+
   )
 }
